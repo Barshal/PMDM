55d901bdbf002b477bba58017712db54cf82eb99
Barshal <hector_mur@hotmail.es>
2026-01-15 15:48:19 +0100

    Navegacion corregida, faltan los menus. Actualmente son botones

--- ActEntregable2/app/src/main/java/com/example/entergable2/SecondActivity.kt
+++ ActEntregable2/app/src/main/java/com/example/entergable2/SecondActivity.kt
@@ -1,18 +1,25 @@
 package com.example.entergable2
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.entergable2.adapter.CarritoAdapter
 import com.example.entergable2.databinding.ActivitySecondBinding
 import com.example.entergable2.model.Product
 import com.google.gson.Gson
 import org.json.JSONArray
 import org.json.JSONObject
+import kotlin.math.round
 
 class SecondActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivitySecondBinding
+    private lateinit var adapter: CarritoAdapter
+
     // Creamos una lista para guardar el carrito que recibimos
     private val carritoRecibido = mutableListOf<Product>()
     private var total = 0.0
@@ -22,19 +29,43 @@ class SecondActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivitySecondBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 1. INICIALIZA el RecyclerView con un adapter vacío
+        initRecyclerView()
+
+        // 2. RECUPERA Y PROCESA los datos del carrito
         recuperarProcesarCarrito()
 
         acciones()
+
     }
 
     private fun acciones() {
-        binding.btnNavMain.setOnClickListener (this)
+        binding.btnNavMain.setOnClickListener(this)
+        binding.btnVaciarCarrito.setOnClickListener(this)
+        binding.btnComprar.setOnClickListener(this)
     }
 
     override fun onClick(v: View?) {
         when (v?.id) {
             binding.btnNavMain.id -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
 
+            binding.btnVaciarCarrito.id -> {
+                carritoRecibido.clear()
+                adapter.actualizarLista(carritoRecibido)
+                total = 0.0
+                binding.txtTotalCarrito.text = round(total).toString() + " €"
+            }
+
+            binding.btnComprar.id -> {
+                Toast.makeText(
+                    this,
+                    "Enhorabuena, compra por valor de ${total} realizada",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
             }
         }
     }
@@ -53,7 +84,24 @@ class SecondActivity : AppCompatActivity(), View.OnClickListener {
             val productoJson: JSONObject = carritoArray.getJSONObject(i)
             val producto: Product = gson.fromJson(productoJson.toString(), Product::class.java)
             carritoRecibido.add(producto)
-            Log.v("carrito",producto.title.toString())
+            if (producto.price != null) {
+                total += producto.price
+            } else {
+                total += 0
+            }
+            Log.v("carrito", producto.title.toString())
         }
+        adapter.actualizarLista(carritoRecibido)
+        binding.txtTotalCarrito.text = round(total).toString() + " €"
+
     }
+
+    private fun initRecyclerView() {
+        // El adapter se crea UNA SOLA VEZ con una lista vacía.
+        adapter = CarritoAdapter(mutableListOf(), this)
+        binding.recyclerViewProductosCarrito.adapter = adapter
+        binding.recyclerViewProductosCarrito.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+
 }
---
+++ ActEntregable2/app/src/main/java/com/example/entergable2/adapter/CarritoAdapter.kt
@@ -0,0 +1,52 @@
+package com.example.entergable2.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.entergable2.R
+import com.example.entergable2.databinding.ItemRecyclerProductoCarritoBinding
+import com.example.entergable2.model.Product
+
+class CarritoAdapter(var lista: List<Product>, var contexto: Context) :
+    RecyclerView.Adapter<CarritoAdapter.MyHolder>() {
+    inner class MyHolder(var binding: ItemRecyclerProductoCarritoBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    // Numero de filas que se pintan
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
+    // Crea el holder, el patron de las filas con la inner clas que a su vez utiliza el xml creado
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyHolder {
+        var binding: ItemRecyclerProductoCarritoBinding =
+            ItemRecyclerProductoCarritoBinding.inflate(
+                LayoutInflater.from(contexto), parent, false
+            )
+        return MyHolder(binding)
+    }
+
+    //Renderiza los elementos
+    override fun onBindViewHolder(
+        holder: MyHolder,
+        position: Int
+    ) {
+        val item: Product = lista[position]
+        holder.binding.txtProductTitle.text = item.title
+        Glide.with(contexto).load(item.thumbnail).placeholder(R.drawable.ic_launcher_background)
+            .into(holder.binding.iamgeRecycler)
+    }
+
+    fun actualizarLista(nuevaLista: List<Product>) {
+        (this.lista as MutableList<Product>).clear()
+        (this.lista as MutableList<Product>).addAll(nuevaLista)
+        notifyDataSetChanged() // ¡Esta línea le dice al RecyclerView que se redibuje!
+    }
+}
+
+
